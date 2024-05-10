8c1bb367cd81c888d0f6f72a376bc2b6630049cc
spacescape <15368104568@189.cn>
2024-05-10 04:40:32 -0700

    Update returnn.c

--- Robot2/return/returnn.c
+++ Robot2/return/returnn.c
@@ -5,13 +5,13 @@ int main(){
     float t1 , t2;
  return [wx,sin,cos,t1,t2]
 }
-int 'point'{
-     int 'point' t1
-     int 'point' t2
+int point{
+     int point t1;
+     int point t2;
 }
 void 'returnn'{
-        return A * 'sin'(wx + t1) + B * 'cos'(wx - t2)
-        return B * 'sin'(wx - t2) + A * 'cos'(wx + t1)
-        return A * 'cos'(wx - t1) + B * 'cos'(wx + t2)
-        return B * 'sin'(wx + t2) + A * 'sin'(wx - t1)
+        return A * sin(wx + t1) + B * cos(wx - t2)
+        return B * sin(wx - t2) + A * cos(wx + t1)
+        return A * cos(wx - t1) + B * cos(wx + t2)
+        return B * sin(wx + t2) + A * sin(wx - t1)
 }

c25808a636d99239526abd06f296c02dabd3afa0
spacescape <15368104568@189.cn>
2024-05-10 04:40:53 -0700

    Update returnn.c

--- Robot2/return/returnn.c
+++ Robot2/return/returnn.c
@@ -9,7 +9,7 @@ int point{
      int point t1;
      int point t2;
 }
-void 'returnn'{
+void returnn(){
         return A * sin(wx + t1) + B * cos(wx - t2)
         return B * sin(wx - t2) + A * cos(wx + t1)
         return A * cos(wx - t1) + B * cos(wx + t2)

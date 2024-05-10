b24442633be98b680cc410f1cc61bc43bb6db477
spacescape <15368104568@189.cn>
2024-05-10 04:33:31 -0700

    Update lmn.c

--- Robot2/crak/lmn.c
+++ Robot2/crak/lmn.c
@@ -1,22 +1,22 @@
 void lmn(){
   int t3(){
   int32_t
-  'asin'(wx + l)
+  asin(wx + l)
   int64_t
-  'acos'(wx - m)
+  acos(wx - m)
   int128_t
-  'asin'(wx + n)
+  asin(wx + n)
   int256_t
-  'acos'(wx - n)
+  acos(wx - n)
   }
   int t4(){
   int256_t
-  'asin'(wx - l)
+  asin(wx - l)
   int128_t
-  'acos'(wx + m)
+  acos(wx + m)
   int64_t
-  'asin'(wx - n)
+  asin(wx - n)
   int32_t
-  'acos'(wx + n)
+  acos(wx + n)
   }
 }

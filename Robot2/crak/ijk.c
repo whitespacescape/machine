73751657ca11a3431df417e6b644f82932252364
spacescape <15368104568@189.cn>
2024-05-10 04:32:38 -0700

    Update ijk.c

--- Robot2/crak/ijk.c
+++ Robot2/crak/ijk.c
@@ -1,21 +1,21 @@
 void ijk{
  int t1(){
     int2_t
-    'sin'(wx + i)
+    sin(wx + i)
     int4_t
-    'cos'(wx - j)
+    cos(wx - j)
     int8_t
-    'sin'(wx + k)
+    sin(wx + k)
     int16_t
-    'cos'(wx - k)
+    cos(wx - k)
 }
 int t2(){
     int16_t
-    'sin'(wx - i)
+    sin(wx - i)
     int8_t
-    'cos'(wx + j)
+    cos(wx + j)
     int4_t
-    'sin'(wx - k)
+    sin(wx - k)
     int2_t
-    'cos'(wx + k)
+    cos(wx + k)
 }}

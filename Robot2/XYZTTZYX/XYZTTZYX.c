d808cfc7410e738964f44102e88d9de2a65aac67
spacescape <15368104568@189.cn>
2024-05-10 04:29:47 -0700

    Update XYZTTZYX.c

--- Robot2/XYZTTZYX/XYZTTZYX.c
+++ Robot2/XYZTTZYX/XYZTTZYX.c
@@ -1,9 +1,9 @@
-void  'XYZTTZYX'(){
-    void 'XYZT'(){
+void  XYZTTZYX(){
+    void XYZT(){
          X1Y1Z1T1<->X2Y2Z2T2<->X3Y3Z3T3
          return t7
     }
-    void 'TZYX'(){
+    void TZYX(){
         Z4Y4X4T4<->Z5Y5X5T5<->Z6Y6X6T6
         return t7
     }

4766f26568866c59ee63844d107faeb592f597a2
spacescape <15368104568@189.cn>
2024-05-10 04:37:51 -0700

    Update f.c

--- Robot2/efgh/f.c
+++ Robot2/efgh/f.c
@@ -2,44 +2,46 @@
 #define v2 7
 #define v3 8
 #define v4 9
+
 #define m1 1
 #define m2 2
 #define m3 3
 #define m4 4
+
 #define c 299792458
-#include "../Header/efgh.h"
-void 'det'{
+enum det{
     enum m1
     enum m2
     enum m3
     enum m4
-    return det
-void 'trace'{
+}
+enum trace{
     inline m1 * m2
     extern m2 * m3
     inline m3 * m4
     extern m2 * m3 *m4
-    return trace
 }
-void 'f1'{
+void 'f1'
 do {
 'A' <- 'B' <-> 'C' <-> 'D'
 (0.25f, 0.5f, 0.75f, 1.0f)
 while ("scalar")
+}
 
-void 'f2'{
+void 'f2'
 do {
 'A' -> 'B' <-> 'C' <-> 'D'
 (4F,3F,3F,1F)
 }
 while ("vector")
+
 void 'f3'
 do {
 'A' <-> 'B' -> 'C' <-> 'D'
-(0.25f , 0.5f , 0.75f  ,1.0f   )
+(0.25f , 0.5f , 0.75f  ,1.0f )
 * (4F ,  3F ,  3F  ,  1F )
 ("tensor")
-}
+}}
 
 void 'f4'
 do {

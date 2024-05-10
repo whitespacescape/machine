1a7eb42a81ddd70b60e3281cb7dd6dc11447b0af
spacescape <15368104568@189.cn>
2024-05-10 04:42:38 -0700

    Update returnnnnn.c

--- Robot2/return/returnnnnn.c
+++ Robot2/return/returnnnnn.c
@@ -8,14 +8,14 @@ int main()
     double sh, ch;
     return [www,sh,ch]
 }
-int 't7t8'{
-    int 'manifold' t7
-    int 'manifold' t8
+int t7t8{
+    int manifold t7
+    int manifold t8
 }
-void 'returnnnnn'(){
-   return ijk * sinh('www' + t7) + lmn * cosh('www' - t8)
-   return lmn * sinh('www' - t7) + opq * cosh('www' + t8)
-   return opq * sinh('www' + t7) + ijk * cosh('www' - t8)
-   return ijk * sinh('www' - t7) + lmn * cosh('www' + t8)
+void returnnnnn(){
+   return ijk * sinh(www + t7) + lmn * cosh(www - t8)
+   return lmn * sinh(www - t7) + opq * cosh(www + t8)
+   return opq * sinh(www + t7) + ijk * cosh(www - t8)
+   return ijk * sinh(www - t7) + lmn * cosh(www + t8)
 }
 
